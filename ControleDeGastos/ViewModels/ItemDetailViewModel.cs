e88ab3ace628f974513d189c8471818824c52c37
Gabriel Begueldo <gabrielbeg1@gmail.com>
2021-02-22 21:33:04 -0300

    - Initial project commit

---
+++ ControleDeGastos/ViewModels/ItemDetailViewModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using ControleDeGastos.Models;
+
+namespace ControleDeGastos.ViewModels
+{
+    public class ItemDetailViewModel : BaseViewModel
+    {
+        public Item Item { get; set; }
+        public ItemDetailViewModel(Item item = null)
+        {
+            Title = item?.Text;
+            Item = item;
+        }
+    }
+}
